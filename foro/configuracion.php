f8aeeac3e9907862aaaca10cbdba612e808137c4
newbiex2022 <102439697+newbiex2022@users.noreply.github.com>
2025-02-12 01:24:58 -0500

    Delete foro directory

--- foro/configuracion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$bd_host = "localhost";
-$bd_usuario = "usuario";
-$bd_password = "contraseña";
-$bd_base = "prueba";
-
-$con = mysql_connect($bd_host, $bd_usuario, $bd_password);
-mysql_select_db($bd_base, $con);
-?>
